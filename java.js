b7ebaeabcbfefc78529d6e24077c773328f6bcb0
Megan <mb539815@ohio.edu>
2018-12-11 00:11:14 -0500

    trying out fixes from email

--- java.js
+++ java.js
@@ -31,3 +31,13 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) next = $(this).siblings(':first');
+  next.children(':first-child').clone().appendTo($(this));
+});
+$('.multi-item-carousel .item').each(function(){
+  var prev = $(this).prev();
+  if (!prev.length) prev = $(this).siblings(':last');
+  prev.children(':nth-last-child(2)').clone().prependTo($(this));
+});
